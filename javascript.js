1aaee672462410a2b7e69806df6a1f018a6ae808
mailinnmoneel <mailinn.kiste@gmail.com>
2021-06-09 13:01:07 +0200

    Oppdatert gruppeoppgave
    Lagt inn HTML og CSS + litt javascript

---
+++ javascript.js
@@ -0,0 +1,28 @@
+// Model
+const pokemon = {
+    WATER:  1,
+    FIRE:   2,
+    EARTH:  3,
+}
+
+
+
+
+// Controller
+
+
+function pickAIPiece()
+{
+    let AIPiece = Math.ceil(Math.random() * 3);
+}
+    
+
+// View
+function updateview(){
+
+
+}
+
+
+
+
